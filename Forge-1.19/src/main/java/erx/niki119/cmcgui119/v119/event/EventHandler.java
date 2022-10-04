b497f53f2f742bdeb5134d35fb89fba638f1da4e
GrigLog <griglogvin@gmail.com>
2022-10-04 19:15:35 +0300

    1.19.2

--- Forge-1.19/src/main/java/erx/niki119/cmcgui119/v119/event/EventHandler.java
+++ Forge-1.19/src/main/java/erx/niki119/cmcgui119/v119/event/EventHandler.java
@@ -3,14 +3,12 @@ package erx.niki119.cmcgui119.v119.event;
 import erx.niki119.cmcgui119.v119.CMCGUI119;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
-import net.minecraftforge.client.event.ScreenOpenEvent;
-import net.minecraftforge.client.loading.ClientModLoader;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class EventHandler {
-
     @SubscribeEvent
-    public void screenOpenEvent(ScreenOpenEvent event){
+    public void screenOpenEvent(ScreenEvent.Opening event){
         if(event.getScreen() != null) {
             CMCGUI119.LOGGER.info(event.getScreen().getClass());
             if(event.getScreen().getClass() == TitleScreen.class) {
@@ -18,7 +16,7 @@ public class EventHandler {
                     try {
                         Screen s = CMCGUI119.INSTANCE.getScreen("MainMenuScreen");
                         if(s != null) {
-                            event.setScreen(s);
+                            event.setNewScreen(s);
                         }
                     }catch(Exception e){
                         e.printStackTrace();
