8e3e56a2a497c3ff9d272c399e85630dfae3f06b
niki119 <niki.1.1.9@yandex.ru>
2023-01-23 06:29:38 +0300

    Улучшение получения данных из жсона

--- Core/src/main/java/erx/niki119/cmcgui119/core/utils/json/components/JsonButton.java
+++ Core/src/main/java/erx/niki119/cmcgui119/core/utils/json/components/JsonButton.java
@@ -5,15 +5,21 @@ public class JsonButton extends JsonComponent {
     public String text;
     public String action;
     public String data;
+    public int fontColor;
     public JsonButton(int xPos, int yPos, int width, int height, String anchor, String text, String texture, String action, String data) {
+        this(xPos, yPos, width, height, anchor, text, texture, action, data, 16777215);
+    }
+
+    public JsonButton(int xPos, int yPos, int width, int height, String anchor, String text, String texture, String action, String data, int fontColor) {
         super("button", xPos, yPos, width, height, anchor);
         this.text = text;
         this.texture = texture;
         this.action = action;
         this.data = data;
+        this.fontColor = fontColor;
     }
 
     public JsonButton(int xPos, int yPos, int width, int height, String anchor, String text, String texture, String action) {
-        this(xPos, yPos, width, height, anchor,text,texture,action,"");
+        this(xPos, yPos, width, height, anchor,text,texture,action,"", 16777215);
     }
 }
